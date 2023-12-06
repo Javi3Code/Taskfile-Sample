2f55b499517d03b57514bbd299888cdcf4ba328e
javi3code <j.code.initialize@gmail.com>
2023-12-06 20:58:31 +0100

    setup: generate ptoyect, taskfile, etc

---
+++ src/main/java/org/jeycode/tskfs/TaskfileSampleApplication.java
@@ -0,0 +1,13 @@
+package org.jeycode.tskfs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskfileSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskfileSampleApplication.class, args);
+	}
+
+}
